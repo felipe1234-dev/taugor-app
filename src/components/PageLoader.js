a82170a2155ee38bc3cc67ef9f1b675fd3e8ce75
Felipe Alves <felipejean2002@gmail.com>
2022-04-13 00:52:09 -0300

    Corrigindo um bug, onde a animação de carregamento muda enquanto o PageLoader está ativo

--- src/components/PageLoader.js
+++ src/components/PageLoader.js
@@ -6,51 +6,59 @@ import MyComponent from "react-fullpage-custom-loader";
 import { getEnv } from "../functions";
 
 // Configs
-const LoadingAnimation = props => <MyComponent {...props}/>;
-const loadTypes = [ 
-    "square-spin", 
-    "ball-newton-cradle",
-    "ball-scale-ripple-multiple",
-    "square-jelly-box",
-    "ball-climbing-dot",
-    "ball-atom",
+const LoadingAnimation = (props) => <MyComponent {...props} />;
+const loaderTypes = [
+	"square-spin",
+	"ball-newton-cradle",
+	"ball-scale-ripple-multiple",
+	"square-jelly-box",
+	"ball-climbing-dot",
+	"ball-atom",
 ];
 
 function PageLoader() {
-    const [joke, setJoke] = useState("");
-    const loadTypeIndex = Math.round(Math.random()*(loadTypes.length - 1));
-    
-    useEffect(() => {
-        const fetchJoke = async () => {
-            /* Fiz rapidamente um CORS proxy clonando um repositório,
-             * porque, o dono da API não colocou "Access-Control-Allow-Origin: *"
-             * no header dele. :/
-             */
-            
-            fetch(getEnv("CORS_PROXY") + getEnv("JOKES_API"))
-            .then((resp) => resp.json())
-            .then((resp) => setJoke(resp.question+" "+resp.answer))
-            .catch((error) => console.error("Error: ", error));
-        }
-        fetchJoke();
-    }, []);
-    
-    const props = {
-        loadingAnim: {
-            loaderType: loadTypes[loadTypeIndex],
-            sentences: [joke],
-            height: "100vh",
-            width: "100%",
-            fadeIn: true,
-            wrapperBackgroundColor: `var(--background)`,
-            color: "var(--blue)",
-            textStyles: {
-                color: "var(--gray-dark)"
-            }
-        }
-    }
-    
-    return joke && <LoadingAnimation {...props.loadingAnim}/>;
+	const [joke, setJoke] = useState("");
+	const [loaderType, setLoaderType] = useState(null);
+
+	useEffect(() => {
+		let chosenLoader = Math.round(Math.random() * (loaderTypes.length - 1));
+		chosenLoader = loaderTypes[chosenLoader];
+
+		setLoaderType(chosenLoader);
+	}, []);
+
+	useEffect(() => {
+		const fetchJoke = async () => {
+			/* Fiz rapidamente um CORS proxy clonando um repositório,
+			 * porque, o dono da API não colocou "Access-Control-Allow-Origin: *"
+			 * no header dele. :/
+			 */
+
+			fetch(getEnv("CORS_PROXY") + getEnv("JOKES_API"))
+				.then((resp) => resp.json())
+				.then((resp) => setJoke(resp.question + " " + resp.answer))
+				.catch((error) => console.error("Error: ", error));
+		};
+
+		fetchJoke();
+	}, []);
+
+	const props = {
+		loadingAnim: {
+			loaderType: loaderType,
+			sentences: [joke],
+			height: "100vh",
+			width: "100%",
+			fadeIn: true,
+			wrapperBackgroundColor: `var(--background)`,
+			color: "var(--blue)",
+			textStyles: {
+				color: "var(--gray-dark)",
+			},
+		},
+	};
+
+	return (loaderType && joke) && <LoadingAnimation {...props.loadingAnim} />;
 }
 
 export default PageLoader;
