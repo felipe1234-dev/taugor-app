31c8042ef379915e091a586d0e972cd580812b7d
Felipe Alves <felipejean2002@gmail.com>
2022-04-17 02:45:59 -0300

    Adicionando controle de path

--- src/contexts/Page.tsx
+++ src/contexts/Page.tsx
@@ -1,20 +1,25 @@
 import React, { useState, createContext } from "react";
 
 export interface PageValue {
+    pathname: string,
+    setPathname(pathname: string): void,
     isLoading: boolean,
     setIsLoading(isLoading: boolean): void
 };
 
-export const PageContext = createContext<PageValue>({ 
+export const PageContext = createContext<PageValue>({
+    pathname: "/",
+    setPathname: () => {},
     isLoading: false, 
     setIsLoading: () => {}
 });
 
 export function PageProvider(props: { children: React.ReactNode }) {
+    const [pathname, setPathname]   = useState<string>("/");
 	const [isLoading, setIsLoading] = useState<boolean>(true); 
     
     return (
-        <PageContext.Provider value={{ isLoading, setIsLoading }}>
+        <PageContext.Provider value={{ pathname, setPathname, isLoading, setIsLoading }}>
             {props.children}
         </PageContext.Provider>
     );
