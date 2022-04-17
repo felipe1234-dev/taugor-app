5fc5f05c2a7b43aa86a9940bc6f0b24ad191366c
Felipe Alves <felipejean2002@gmail.com>
2022-04-17 19:06:48 -0300

    Exportando a página de erro 404

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,4 @@
 export { default as HomePage } from "./home/HomePage";
 export { default as LoginPage } from "./login/LoginPage";
 export { default as TaskPage } from "./task/TaskPage";
-export { }
+export { default as Error404Page } from "./error-404/Error404Page";
