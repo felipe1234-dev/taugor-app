bf73c3a3c53d0f7f958c0fe5ac031b44548a211f
Felipe Alves <felipejean2002@gmail.com>
2022-04-17 07:30:44 -0300

    Com components/RequireAuth, contexts/Page não é mais preciso

--- src/contexts/MasterProvider.tsx
+++ src/contexts/MasterProvider.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from "react";
 import { FirebaseProvider } from "./Firebase";
-import { PageProvider } from "./Page";
 import { UserProvider } from "./User";
 import { ThemeProvider } from "./Theme";
 import { AlertProvider } from "./Alert";
@@ -21,7 +20,6 @@ const ProviderCompose = (props: ProviderComposeProps) => (
 export const MasterProvider = (props: { children: ReactNode }) => (
     <ProviderCompose providers={[
         FirebaseProvider,
-        PageProvider,
         UserProvider,
         ThemeProvider,
         AlertProvider
--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -1,6 +1,5 @@
 export * from "./Alert";
 export * from "./Firebase";
 export * from "./MasterProvider";
-export * from "./Page";
 export * from "./Theme";
 export * from "./User";
