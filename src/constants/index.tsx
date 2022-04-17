f97ee1e5a2de65d201ce8a71067055c02ae8e468
Felipe Alves <felipejean2002@gmail.com>
2022-04-16 22:40:33 -0300

    Criando os tipos do projeto

---
+++ src/constants/index.tsx
@@ -0,0 +1,53 @@
+// Libs
+import {
+    LowPriority as LowPriorityIcon,
+    GradeRounded as MediumPriorityIcon,
+    PriorityHigh as HighPriorityIcon,
+    CheckCircleOutlineRounded as FinishedIcon,
+    Autorenew as InProgressIcon,
+    AssessmentRounded as InAnalysisIcon
+} from "@mui/icons-material";
+
+// Images
+import WhiteTaugorLogo from "@local/media/white-taugor-logo.png";
+import BlueTaugorLogo from "@local/media/blue-taugor-logo.png";
+import WhiteTaugorIcon from "@local/media/white-taugor-icon.png";
+
+export const APP_INFO = { 
+    appName: "Gestor de Atividades Taugor",
+	catchline: "Gerencie as suas atividades dentro da empresa",
+	whiteLogo: WhiteTaugorLogo,
+	blueLogo: BlueTaugorLogo,  
+	whiteIcon: WhiteTaugorIcon,
+	alt: "Taugor Corporation",
+	link: "http://www.taugor.com.br/"
+};
+
+export const STATUS_TYPES = [
+	"Em análise",
+	"Em andamento",
+	"Finalizado"
+] as const;
+
+export const STATUS_ICONS = {
+    "Finalizado": <FinishedIcon />,
+    "Em andamento": <InProgressIcon />, 
+    "Em análise": <InAnalysisIcon />
+};
+
+export const PRIORITY_TYPES = [
+    "Baixa",
+	"Média",
+	"Alta"
+] as const;
+
+export const PRIORITY_ICONS = {
+    "Baixa": <LowPriorityIcon />,
+    "Média": <MediumPriorityIcon />,
+    "Alta": <HighPriorityIcon />
+};
+
+export const ENV_TYPES = [
+    "Ambiente de Produção - Cliente Ativo/Licença",
+    "Dados/Ambiente de Testes - Somente testes"
+] as const;
