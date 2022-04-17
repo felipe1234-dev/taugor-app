d9b999bd4ff73018afc3ca28fce857a041f9a130
Felipe Alves <felipejean2002@gmail.com>
2022-04-16 22:29:29 -0300

    Convertendo LoginPage para Typescript

---
+++ src/pages/login/LoginPage.tsx
@@ -0,0 +1,104 @@
+// Libs
+import { useEffect } from "react";
+import { 
+	Grid, 
+	Container, 
+	Box, 
+	Typography 
+} from "@mui/material";
+import Tilt from "react-parallax-tilt";
+
+// Page components
+import LoginForm from "./LoginForm";
+
+// Interfaces
+import { Page } from "@local/interfaces";
+ 
+// Style
+import "@local/style/pages/LoginPage.scss";
+
+// Constants 
+import { APP_INFO } from "@local/constants";
+
+const {
+	appName, 
+	catchline, 
+	whiteLogo, 
+	alt, 
+	link 
+} = APP_INFO;
+
+export default function LoginPage({ title }: Page) {
+	useEffect(() => {
+        document.title = title;
+    }, [title]);
+
+    const center = {
+        justifyContent: "center",
+        alignItems: "center"
+    }
+
+    const container = {
+        className: "LoginPage",
+        component: "main",
+        maxWidth: "xs" as "xs"
+    }
+
+    const box = {
+        sx: {
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center"
+        }
+    }
+
+    const logo = {
+        className: "LoginPage-logo",
+        src: whiteLogo,
+        alt: alt,
+        onClick: () => window.open(link, "_blank")?.focus()
+    }
+
+    const logoTilt = {
+        tiltMaxAngleX: 40,
+        tiltMaxAngleY: 40,
+        perspective: 800,
+        transitionSpeed: 1500,
+        scale: 1.1,
+        gyroscope: true,
+    }
+
+    const siteTitle = {
+        className: "LoginPage-appName",
+        component: "h1",
+        variant: "h5" as "h5"
+    }
+
+    const catchTitle = {
+        className: "LoginPage-catchline",
+        component: "h2",
+        variant: "h6" as "h6"
+    }
+
+	return ( 
+		<Grid {...center}>
+			<Grid item>
+				<Container {...container}>
+					<Box {...box}>
+						<Tilt {...logoTilt}>
+							<img {...logo}/>
+						</Tilt>
+						<Typography {...siteTitle}>
+							{appName}
+						</Typography>
+						<Typography {...catchTitle}>
+							{catchline}
+						</Typography>
+						<LoginForm />
+					</Box>
+				</Container>
+			</Grid>
+		</Grid>
+	);
+};
