1521f2c491bbc4d46c96a686090a21bbe17f2c8c
Felipe Alves <felipejean2002@gmail.com>
2022-04-17 19:04:57 -0300

    Componente de página não encontrada completo

--- src/pages/error-404/Error404Page.tsx
+++ src/pages/error-404/Error404Page.tsx
@@ -1,8 +1,32 @@
-// Interfaces
-import 
+// Libs
+import { Box, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+// Media
+import PageNotFoundImg from "@local/media/page-not-found.svg";
 
 export default function Error404Page() {
     return (
-        
+        <Box 
+            display="flex"
+            flexDirection="column" 
+			alignItems="center"
+			justifyContent="center"
+			sx={{ minWidth: "100%" }}
+        >
+			<img
+                style={{ minHeight: "100vh" }}
+                src={PageNotFoundImg}
+                alt="Página não encontrada"
+            />
+			<Typography variant="h5">
+                Awww... Essa não!
+            </Typography>
+            <Typography variant="body1">
+                A página que você estava procurando não existe! 
+                Mas não criemos pânico! Você pode voltar para o 
+                <Link to="/" replace>início</Link>
+            </Typography>
+		</Box>
     );
 };
