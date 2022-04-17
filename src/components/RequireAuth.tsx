10d5bf44e270e83b658a450ecaf4ebbca0a2b6db
Felipe Alves <felipejean2002@gmail.com>
2022-04-17 07:29:02 -0300

    Inspirado no exemplo: https://stackblitz.com/github/remix-run/react-router/tree/main/examples/auth?file=src/App.tsx

---
+++ src/components/RequireAuth.tsx
@@ -0,0 +1,63 @@
+// Libs
+import { 
+    useState, 
+    useEffect, 
+    useContext 
+} from "react";
+import { Navigate, useLocation } from "react-router-dom";
+
+// API Firebase
+import { getCurrentUser, logout } from "@local/api/auth/sign-in";
+
+// Contexts
+import { FirebaseContext, UserContext } from "@local/contexts";
+
+// Props interface
+interface RequireAuthProps {
+    children: JSX.Element
+};
+
+export default function RequireAuth({ children }: RequireAuthProps) {
+    const [allowed, setAllowed] = useState<boolean>(false);
+    const location = useLocation();
+    
+    const { db } = useContext(FirebaseContext);
+    const { setUser } = useContext(UserContext);
+    
+    useEffect(() => {
+        const refreshToken = sessionStorage.getItem("Auth Token");
+
+        if (!!refreshToken) {
+            const tokenDate     = Number(sessionStorage.getItem("Assign Date"));
+            const timeNow       = new Date().getTime();
+            const minutesPassed = (timeNow - tokenDate)/(1000 * 60);
+
+            if (minutesPassed > 60) {
+                logout().then(() => {
+                    sessionStorage.removeItem("Auth Token");
+                    sessionStorage.removeItem("Assign Date");
+                });
+            }
+        }
+
+        const fetchUserData = async() => {
+            const user = await getCurrentUser(db);
+
+            if (!!refreshToken && !!user) {
+                setUser(user);
+                setAllowed(true);
+            } else {
+                setUser(null);
+                setAllowed(false);
+            }
+        }
+
+        fetchUserData();
+    }, [db]);
+
+    if (!allowed) {
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+    return children;
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export { default as AlertMessage } from "./AlertMessage";
 export { default as FileViewer } from "./FileViewer";
 export { default as PageLoader } from "./PageLoader";
 export { default as ProfileImage } from "./ProfileImage";
+export { default as RequireAuth } from "./RequireAuth";
 export { default as SearchBar } from "./SearchBar";
 export { default as Spinner } from "./Spinner";
 export { default as TextEditor } from "./TextEditor";
