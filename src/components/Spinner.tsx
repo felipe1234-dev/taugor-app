4218afcaeb39191039d9a13c1f675b891469c637
Felipe Alves <felipejean2002@gmail.com>
2022-04-19 16:04:48 -0300

    wrapper e spinner agora aceitam quaisquer propriedades CSS

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,42 +1,27 @@
 // Libs
+import { CSSProperties } from "react";
 import { Box } from "@mui/material";
 
 // Styles
 import "@local/style/components/Spinner.scss";
 
 interface SpinnerProps {
-    wrapper: {
-        width?: string|number,
-        height?: string|number
-    },
-    spinner: { 
-        width?: string|number,
-        height?: string|number,
-        barColor?: string,
-        pathColor?: string
-    }
+    wrapper?: CSSProperties,
+    spinner?: CSSProperties
 };
 
-export default function Spinner({ wrapper, spinner }: SpinnerProps) {
+export default function Spinner({ wrapper = {}, spinner = {} }: SpinnerProps) {
     return (
         <Box
             className="Spinner-wrapper"
             display="flex"
 			alignItems="center"
 			justifyContent="center"
-			sx={{ 
-                width: wrapper.width? spinner.width : "", 
-                minHeight: wrapper.height? spinner.height : ""
-            }}
+			sx={wrapper}
         >
             <div
                 className="Spinner"
-                style={{
-                    width: spinner.width? spinner.width : "",
-                    height: spinner.height? spinner.height : "",
-                    ["--bar-color" as string]: spinner.barColor? spinner.barColor : "var(--bar-color)",
-                    ["--path-color" as string]: spinner.pathColor? spinner.pathColor : "var(--path-color)"
-                }}
+                style={spinner}
             ></div>
         </Box>
     );
