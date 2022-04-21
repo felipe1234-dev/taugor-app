c418e67e29de9e3a903ed04066f47bd9241eaae7
Felipe Alves <felipejean2002@gmail.com>
2022-04-21 03:54:08 -0300

    Criando lógica de persistência de autenticação com sessionStorage, refreshToken e onAuthStateChanged

--- src/api/auth/getCurrentUser.ts
+++ src/api/auth/getCurrentUser.ts
@@ -1,27 +1,44 @@
 // Firebase
-import { getAuth } from "@firebase/auth";
+import { getAuth, onAuthStateChanged } from "@firebase/auth";
 import { Firestore } from "@firebase/firestore";
 
 // Local
 import { User } from "@local/interfaces";
-import { Alert } from "@local/types";
 import { getUserByUuid } from "../collections/Users";
+import logOut from "./logOut";
 
-export default function getCurrentUser(db: Firestore): Promise<User|Alert> {
+export default function getCurrentUser(db: Firestore): Promise<User|null> {
     return new Promise((resolve, reject) => {
-        const auth     = getAuth();
-        const currUser = auth.currentUser;
-        
-        if (!!currUser) {
-            const { uid } = currUser; 
-            
-            getUserByUuid(db, uid)
-                .then((user) => (
-                    resolve(user)
-                ))
-                .catch((error) => (
-                    reject(error)
-                ));
+        const auth = getAuth();
+        const refreshToken = sessionStorage.getItem("Auth Token");
+
+        if (!!refreshToken) {
+            const tokenDate = Number(sessionStorage.getItem("Assign Date"));
+            const timeNow = new Date().getTime();
+            const minutesPassed = (timeNow - tokenDate) / (1000 * 60);
+
+            if (minutesPassed > 60) {
+                logOut()
+                    .catch((error) => (
+                        reject(error)
+                    ));
+            }
         }
+        
+        onAuthStateChanged(auth, (user) => {
+            if (!!user && !!sessionStorage.getItem("Auth Token")) {
+                const { uid } = user; 
+                
+                getUserByUuid(db, uid)
+                    .then((user) => (
+                        resolve(user)
+                    ))
+                    .catch((error) => (
+                        reject(error)
+                    ));
+            } else {
+                resolve(user as User|null);
+            }
+        });        
     });
 };
