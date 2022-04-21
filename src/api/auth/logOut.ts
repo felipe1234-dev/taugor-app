520260ffcc2e3085189ff0a9004e6d94dd62a173
Felipe Alves <felipejean2002@gmail.com>
2022-04-21 04:00:32 -0300

    Remove o refresh token ao fazer logout

--- src/api/auth/logOut.ts
+++ src/api/auth/logOut.ts
@@ -2,14 +2,19 @@ import { FirebaseError } from "firebase/app";
 import { getAuth, signOut } from "firebase/auth";
 import toAlert from "../toAlert";
 
-export default function logOut() {
+export default function logOut(): Promise<void> {
     return new Promise((resolve, reject) => {
         const auth = getAuth();
         
         signOut(auth)
-            .then(resolve)
+            .then(() => {
+                sessionStorage.removeItem("Auth Token");
+                sessionStorage.removeItem("Assign Date");
+                
+                resolve();
+            })
             .catch((error: FirebaseError) => (
                 reject(toAlert(error)) 
             ));
-    });
+    }); 
 };
