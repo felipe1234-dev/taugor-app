0e991b502394da147e0cee2871b26b21cdd46f3e
Felipe Alves <felipejean2002@gmail.com>
2022-04-22 15:04:36 -0300

    Indentando com 4 espaços

--- src/components/PageLoader.tsx
+++ src/components/PageLoader.tsx
@@ -1,8 +1,8 @@
 // Libs
-import { 
-	useState, 
-	useEffect,
-	useContext
+import {
+    useState,
+    useEffect,
+    useContext
 } from "react";
 import { default as LoadingAnimation } from "react-fullpage-custom-loader";
 
@@ -17,69 +17,68 @@ import "@local/style/components/PageLoader.scss";
 
 // Configs
 const loaders = [
-	"square-spin",
-	"ball-newton-cradle",
-	"ball-scale-ripple-multiple",
-	"square-jelly-box",
-	"ball-climbing-dot",
-	"ball-atom",
+    "square-spin",
+    "ball-newton-cradle",
+    "ball-scale-ripple-multiple",
+    "square-jelly-box",
+    "ball-climbing-dot",
+    "ball-atom",
 ];
 
 export default function PageLoader() {
-	const [joke, setJoke]     = useState<string>("");
-	const [loader, setLoader] = useState<string|null>(null);
+    const [joke, setJoke] = useState<string>("");
+    const [loader, setLoader] = useState<string | null>(null);
 
     const { setSeverity, setMessage } = useContext(AlertContext);
-	
-	useEffect(() => {
-		const randIndex = Math.round(Math.random() * (loaders.length - 1));
-		const selLoader = loaders[randIndex];
-		
-		setLoader(selLoader); 
-	}, []);
 
-	useEffect(() => {
-		/* Fiz rapidamente um CORS proxy clonando um repositório,
-		 * porque, o dono da API não colocou "Access-Control-Allow-Origin: *"
-		 * no header dele. :/
-		 */
-            
-		const corsProxy = getEnv("CORS_PROXY");
-		const jokesApi = getEnv("JOKES_API");
+    useEffect(() => {
+        const randIndex = Math.round(Math.random() * (loaders.length - 1));
+        const selLoader = loaders[randIndex];
 
-		if (!!corsProxy && !!jokesApi) {
-			const apiUrl = corsProxy + jokesApi;
+        setLoader(selLoader);
+    }, []);
 
-			fetch(apiUrl)
-				.then((resp) => resp.json())
-				.then((resp) => setJoke(resp.question + " " + resp.answer))
-				.catch((error: Error) => {
-					setSeverity("error" as "error");
-					setMessage("Erro desconhecido: " + error.message);
-				});
-		}
-	}, []);
+    useEffect(() => {
+        /* Fiz rapidamente um CORS proxy clonando um repositório,
+         * porque, o dono da API não colocou "Access-Control-Allow-Origin: *"
+         * no header dele. :/
+         */
 
-	const loadingAnimation = {
-		loader: "ball-newton-cradle",
-		sentences: [joke],
-		height: "100vh",
-		width: "100%",
-		fadeIn: true,
-		wrapperBackgroundColor: `var(--wrapper-color)`,
-		color: "var(--loader-color)",
-		textStyles: {
-			color: "var(--text-color)",
-		}
-	};
-	
-	return (
-		<div className="PageLoader">
-			{(!!loader && !!joke)? (
-				<LoadingAnimation {...loadingAnimation} />
-			) : (
-				<span></span>
-			)}
-		</div>
-	);
+        const corsProxy = getEnv("CORS_PROXY");
+        const jokesApi = getEnv("JOKES_API");
+
+        if (!!corsProxy && !!jokesApi) {
+            const apiUrl = corsProxy + jokesApi;
+
+            fetch(apiUrl)
+                .then((resp) => resp.json())
+                .then((resp) => setJoke(resp.question + " " + resp.answer))
+                .catch((error: Error) => {
+                    setSeverity("error");
+                    setMessage("Erro desconhecido: " + error.message);
+                });
+        }
+    }, []);
+
+    const loadingAnimation = {
+        loader: loader,
+        sentences: [joke],
+        height: "100vh",
+        width: "100%",
+        fadeIn: true,
+        wrapperBackgroundColor: `var(--wrapper-color)`,
+        color: "var(--loader-color)",
+        textStyles: {
+            color: "var(--text-color)",
+        }
+    };
+
+    return (
+        <div className="PageLoader">
+            {(!!loader && !!joke) && (<>
+            {loader}
+                <LoadingAnimation {...loadingAnimation} />
+            </>)}
+        </div>
+    );
 };
