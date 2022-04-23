5eecd452adbac779150c66c90b5cbcd630ae114c
Felipe Alves <felipejean2002@gmail.com>
2022-04-23 20:04:35 -0300

    Implementando uma verificação de tipo com props opcionais

--- src/functions/isRouteState.ts
+++ src/functions/isRouteState.ts
@@ -2,9 +2,19 @@ import { RouteState } from "@local/types";
 import isLocation from "./isLocation";
 
 export default function isRouteState(object: any): object is RouteState {
-    return (
-        !!object && 
-        (object as RouteState).from !== undefined 
-        && isLocation((object as RouteState).from)
-    );
+    let isRoute: boolean = true;
+    
+    if (!object) {
+        isRoute = false;
+    }
+    
+    if ("from" in object && !isLocation((object as RouteState).from)) {
+        isRoute = false;
+    }
+    
+    if ("openDialog" in object && typeof object.openDialog !== "boolean") {
+        isRoute = false;
+    }
+    
+    return isRoute;
 };
