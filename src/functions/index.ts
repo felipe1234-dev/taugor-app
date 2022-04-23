3e459b425acc501091fec30e35d8edbb9e7386da
Felipe Alves <felipejean2002@gmail.com>
2022-04-23 17:02:14 -0300

    Exportando isLocation e isRouteState

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,5 +1,7 @@
 export { default as getEnv } from "./getEnv";
 export { default as groupDocsByTime } from "./groupDocsByTime";
+export { default as isLocation } from "./isLocation";
+export { default as isRouteState } from "./isRouteState";
 export { default as stringToAvatar } from "./stringToAvatar";
 export { default as stringToColor } from "./stringToColor";
 export { default as translateDate } from "./translateDate";
