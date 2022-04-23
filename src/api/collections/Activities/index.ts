523220977710d39986b5309194ad999eb5d9378d
Felipe Alves <felipejean2002@gmail.com>
2022-04-23 02:27:36 -0300

    Exportando a função de deletar tasks

--- src/api/collections/Activities/index.ts
+++ src/api/collections/Activities/index.ts
@@ -1,2 +1,3 @@
+export { default as deleteActivityByUuid } from "./deleteActivityByUuid";
 export { default as getActivities } from "./getActivities";
 export { default as getActivityByUuid } from "./getActivityByUuid";
