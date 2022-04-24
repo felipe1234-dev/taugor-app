c84df4b391b68aa68602a979ed0ca708295cf79e
Felipe Alves <felipejean2002@gmail.com>
2022-04-24 01:42:29 -0300

    Corrigindo falha na lógica e implemetando switch case para legibilidade

--- src/functions/isRouteState.ts
+++ src/functions/isRouteState.ts
@@ -2,23 +2,13 @@ import { RouteState } from "@local/types";
 import isLocation from "./isLocation";
 
 export default function isRouteState(object: any): object is RouteState {
-    let isRouteState: boolean = true;
-    
-    if (!object) {
-        isRouteState = false;
+    switch(true) {
+        case !object || typeof object !== "object":
+        case "from" in object && !isLocation((object as RouteState).from):
+        case "background" in object && !isLocation((object as RouteState).background):
+        case !("from" in object && "background" in object):
+            return false;
+        default: 
+            return true;
     }
-    
-    if ("from" in object && !isLocation((object as RouteState).from)) {
-        isRouteState = false;
-    }
-    
-    if ("background" in object && !isLocation((object as RouteState).background)) {
-        isRouteState = false;
-    }
-    
-    if (!("from" in object && "background" in object)) {
-        isRouteState = false;
-    }
-    
-    return isRouteState;
 };
