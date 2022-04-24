bb4d1a062d3b4d950628c2b3b3998971bfec5b60
Felipe Alves <felipejean2002@gmail.com>
2022-04-24 00:08:54 -0300

    Criando diálogo de deletar tasks

---
+++ src/dialogs/delete/DeleteDialog.tsx
@@ -0,0 +1,100 @@
+// Libs
+import { 
+    useState, 
+    useEffect, 
+    useContext 
+} from "react";
+import {
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogContentText,
+    DialogActions,
+    Button
+} from "@mui/material";
+import { 
+    Link, 
+    useNavigate, 
+    useParams 
+} from "react-router-dom";
+
+// Contexts
+import { AlertContext, FirebaseContext } from "@local/contexts";
+
+// API
+import { 
+    deleteActivityByUuid, 
+    getActivityByUuid 
+} from "@local/api/collections/Activities";
+
+// Interfaces
+import { Task } from "@local/interfaces";
+
+export default function DeleteDialog() {
+    const [task, setTask] = useState<Task|null>(null);
+    
+    const { setSeverity, setMessage } = useContext(AlertContext);
+    const { db } = useContext(FirebaseContext);
+    
+    const { uuid: taskUuid } = useParams();
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        if (!!taskUuid) {
+            getActivityByUuid(db, taskUuid)
+                .then((task) => (
+                    setTask(task)
+                ))
+                .catch((error) => {
+                    setSeverity(error.severity);
+                    setMessage(error.message); 
+                });
+        }
+    }, [task, taskUuid, db]);
+    
+    const dialog = {
+        open: true
+    }
+    
+    const cancelButton = {
+        component: Link,
+        to: "/",
+        state: {} // Reinicia o estado para não ter nenhum dialog aberto
+    }
+    
+    const deleteButton = {
+        onClick: () => {
+            if (!!task && !!taskUuid) {
+                deleteActivityByUuid(db, task.uuid)
+                    .then(() => (
+                        navigate("/", { replace: true })
+                    ))
+                    .catch((error) => {
+                        setSeverity(error.severity);
+                        setMessage(error.message);
+                    });
+            }
+        }
+    }
+    
+    return (
+        <Dialog {...dialog}>
+            <DialogTitle>
+                Tem certeza que quer excluir?
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    Esta ação não pode ser revertida
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button {...cancelButton}>
+                    Cancelar
+                </Button>
+                <Button {...deleteButton}>
+                    Confirmar
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
