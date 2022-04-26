c3f0b0e976e5deb91e9305a8573239847b79da72
Felipe Alves <felipejean2002@gmail.com>
2022-04-25 22:13:16 -0300

    Transferindo getFileByFilename para um arquivo próprio

---
+++ src/api/storage/attachments/getFileByFilename.ts
@@ -0,0 +1,34 @@
+import { 
+    ref,
+    getDownloadURL,
+    FirebaseStorage
+} from "firebase/storage";
+import { File } from "@local/interfaces"; 
+import toAlert from "@local/api/toAlert";
+
+export default function getFileByFilename(storage: FirebaseStorage, filename: string): Promise<File> {
+    return new Promise((resolve, reject) => {
+        const fileRef  = ref(storage, `attachments/${filename}`);
+        const fileType = filename.match(/\.\w+$/)![0].replace(".", "");
+        const fileName = filename.replace(/-id\d+\.\w+$/, "");
+    
+        if (!!fileRef) {
+            getDownloadURL(fileRef)
+                .then((fileUrl) => (
+                    resolve({
+                        name: fileName,
+                        type: fileType as "pdf"|"txt",
+                        url: fileUrl
+                    })
+                ))
+                .catch((error) => (
+                    reject(toAlert(error))
+                ));
+        }
+        
+        reject({
+            severity: "warning",
+            message: `Anexo ${fileName}.${fileType} não existe`
+        });
+    });
+};
