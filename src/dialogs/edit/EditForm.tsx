a1b3657827a20e8b7562dc8efac7b0b89bf38f84
Felipe Alves <felipejean2002@gmail.com>
2022-04-28 01:25:51 -0300

    Adicionando mais campos

--- src/dialogs/edit/EditForm.tsx
+++ src/dialogs/edit/EditForm.tsx
@@ -8,9 +8,13 @@ import {
     Box,
     TextField,
     DialogContentText,
-    MenuItem
+    MenuItem,
+    Divider
 } from "@mui/material";
 import { useParams } from "react-router-dom";
+
+// Local components
+import { ChipField, TextEditor } from "@local/components"; 
 
 // Contexts
 import { AlertContext, FirebaseContext } from "@local/contexts";
@@ -25,7 +29,9 @@ import { getActivityByUuid } from "@local/api/collections/Activities";
 import { 
     STATUS_TYPES,
     PRIORITY_TYPES,
-    ENV_TYPES
+    ENV_TYPES,
+    INFLUENCED_USERS,
+    TAGS
 } from "@local/constants";
 
 export default function EditForm() {
@@ -74,7 +80,20 @@ export default function EditForm() {
         }
     }
     
-    const baseTextField = {
+    const baseTextField = (
+        name: string, 
+        placeholder: string, 
+        length: number,
+        value: string
+    ) => ({
+        name: name,
+        placeholder: placeholder,
+        defaultValue: value,
+        inputProps: {
+            maxLength: length
+        },
+        
+        required: true,
         maxRows: 4,
         fullWidth: true,
         multiline: true,
@@ -82,67 +101,85 @@ export default function EditForm() {
             mb: 0,
             mt: 2,
         }
-    }
+    });
     
-    const titleTextField = {
-        name: "title",
-        placeholder: "Título",
-        defaultValue: task?.title.join(" "),
-        inputProps: {
-            maxLength: 50
-        }
-    }
-    
-    const briefTextField = {
-        name: "brief",
-        placeholder: "Explicação breve",
-        defaultValue: task?.brief,
-        inputProps: {
-            maxLength: 150
-        }
-    }
-    
-    const baseSelectField = {
+    const baseSelectField = (name: string, value: string) => ({
+        name: name,
+        defaultValue: value,
+        
+        required: true,
         select: true,
         sx: {
             mb: 0,
             mt: 2
         }
+    });
+    
+    const chipField = (name: string, placeholder: string, value: Array<string>) => ({
+        name: name,
+        placeholder: placeholder,
+        defaultValue: value,
+        options: [ ...TAGS ],
+        
+        required: true,
+        maxRows: 4,
+        fullWidth: true,
+        multiline: true
+    });
+    
+    const dialogContentText = {
+        sx: {
+            mt: 2
+        }
     }
     
-    const statusSelectField = {
-        name: "status",
-        defaultValue: task?.status
+    const divider = {
+        sx: {
+            m: 2
+        }
     }
     
-    const prioritySelectField = {
-        name: "priority",
-        defaultValue: task?.priority
-    }
-    
-    const envSelectField = {
-        name: "environment",
-        defaultValue: task?.environment
-    }
+    const textEditor = (initialContent: string) => ({
+        isPreview: false,
+        initialContent: initialContent,
+        onChange: (html: string) => {
+            console.log(html);
+        }
+    });
     
     return (
         <Box {...form}>
-            <DialogContentText>
-                 
-            </DialogContentText>
             {!!task && (
                 <>
+                    {/* TITLE & BRIEF*/}
+                    <DialogContentText {...dialogContentText}>
+                        Título & descrição breve
+                    </DialogContentText>
                     <TextField
-                        {...baseTextField} 
-                        {...titleTextField}
+                        {...baseTextField("title", "Título", 50, task.title.join(" "))}
                     />
                     <TextField
-                        {...baseTextField} 
-                        {...briefTextField}
+                        {...baseTextField("brief", "Explicação breve", 150, task.brief)}
                     />
-                    <TextField 
-                        {...baseSelectField} 
-                        {...statusSelectField}
+                    <Divider {...divider}/>
+                    
+                    {/* DESCRIPTION */}
+                    <TextEditor 
+                        {...textEditor(task.description)}
+                    />
+                    
+                    <Divider {...divider}/>
+                    <ChipField 
+                        {...chipField("tags", "Categorias", task.tags)}
+                    />
+                    <Divider {...divider}/>
+                    
+                    {/* STATUS */}
+                    <DialogContentText {...dialogContentText}>
+                        Situação atual
+                    </DialogContentText>
+                    <TextField
+                        {...baseSelectField("status", task.status)}
                     >
                         {STATUS_TYPES.map((value, i) => (
                             <MenuItem key={i} value={value}>
@@ -150,9 +187,13 @@ export default function EditForm() {
                             </MenuItem>
                         ))}
                     </TextField>
+                    
+                    {/* PRIORITY */}
+                    <DialogContentText {...dialogContentText}>
+                        Nível de urgência
+                    </DialogContentText>
                     <TextField 
-                        {...baseSelectField}
-                        {...prioritySelectField}
+                        {...baseSelectField("priority", task.priority)}
                     >
                         {PRIORITY_TYPES.map((value, i) => (
                             <MenuItem key={i} value={value}>
@@ -160,11 +201,29 @@ export default function EditForm() {
                             </MenuItem>
                         ))}
                     </TextField>
-                    <TextField 
-                        {...baseSelectField}
-                        {...envSelectField}
+                    
+                    {/* ENVIRONMENT */}
+                    <DialogContentText {...dialogContentText}>
+                        Ambiente
+                    </DialogContentText>
+                    <TextField
+                        {...baseSelectField("environment", task.environment)}
                     >
                         {ENV_TYPES.map((value, i) => (
+                            <MenuItem key={i} value={value}>
+                                {value}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                    
+                    {/* INFLUENCED USERS */}
+                    <DialogContentText {...dialogContentText}>
+                        Usuários influenciados
+                    </DialogContentText>
+                    <TextField
+                        {...baseSelectField("influencedUsers", task.influencedUsers)}
+                    >
+                        {INFLUENCED_USERS.map((value, i) => (
                             <MenuItem key={i} value={value}>
                                 {value}
                             </MenuItem>
