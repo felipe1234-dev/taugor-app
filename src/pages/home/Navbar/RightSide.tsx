07393af65607822610909966c881f3d6493326f7
Felipe Alves <felipejean2002@gmail.com>
2022-04-30 00:53:39 -0300

    Corrigindo nome da propriedade

--- src/pages/home/Navbar/RightSide.tsx
+++ src/pages/home/Navbar/RightSide.tsx
@@ -96,7 +96,7 @@ export default function RightSide(user: User) {
         ) : (
             <>
                 <MobileMenu
-                    isOpen={burgerIsOpen}
+                    open={burgerIsOpen}
                     onClose={() => setBurgerIsOpen(false)}
                     {...baseProps}
                     {...user}
