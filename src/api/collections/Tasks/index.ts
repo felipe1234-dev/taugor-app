2d63ac060eae7bde763431ceb7248fe80bff0139
Felipe Alves <felipejean2002@gmail.com>
2022-05-01 20:53:32 -0300

    Exportando updateTaskByUuid

--- src/api/collections/Tasks/index.ts
+++ src/api/collections/Tasks/index.ts
@@ -1,3 +1,4 @@
 export { default as deleteTaskByUuid } from "./deleteTaskByUuid";
 export { default as getTasks } from "./getTasks";
 export { default as getTaskByUuid } from "./getTaskByUuid";
+export { default as updateTaskByUuid } from "./updateTaskByUuid";
