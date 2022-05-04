76af2213c8f0ed7d73eabda488f790a68d95ed74
Felipe Alves <felipejean2002@gmail.com>
2022-05-04 14:30:05 -0300

    Criando função de upload de anexos

--- src/api/storage/attachments/uploadAttach.ts
+++ src/api/storage/attachments/uploadAttach.ts
@@ -1,5 +1,26 @@
-export default function uploadAttach() {
+import { 
+    FirebaseStorage, 
+    ref, 
+    uploadBytes,
+    UploadResult
+} from "firebase/storage";
+import { FirebaseError } from "@firebase/util";
+import toAlert from "@local/api/toAlert";
+
+export default function uploadAttach(storage: FirebaseStorage, file: File): Promise<UploadResult> {
     return new Promise((resolve, reject) => {
+        const fileType = file.name.match(/\.(\w+)$/)![1];
+        const filename = file.name.replace("." + fileType, "");
+        const filenameInStorage = `${filename}-id${file.lastModified}.${fileType}`;
         
+        try {
+            const fileRef = ref(storage, `attachments/${filenameInStorage}`);
+
+            uploadBytes(fileRef, file)
+                .then((snap) => resolve(snap))
+                .catch((error) => reject(toAlert(error)));
+        } catch (error) {
+            reject(toAlert(error as FirebaseError))
+        }
     });
 }
