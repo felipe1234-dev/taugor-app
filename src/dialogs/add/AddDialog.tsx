7ac3868c5a00438c5b4097adcfd1137ada18c656
Felipe Alves <felipejean2002@gmail.com>
2022-05-05 01:58:06 -0300

    Implementando evento onSubmit e chamando TaskForm

--- src/dialogs/add/AddDialog.tsx
+++ src/dialogs/add/AddDialog.tsx
@@ -1,9 +1,57 @@
+// Libs
+import { useContext } from "react";
 import { Dialog } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+
+// Dialog components
 import TaskForm from "../TaskForm";
+
+// Interfaces
+import { Task } from "@local/interfaces";
+
+// API
+import { addTask } from "@local/api/collections/Tasks";
+import { uploadAttach } from "@local/api/storage/attachments";
+
+// Contexts
+import { AlertContext, FirebaseContext } from "@local/contexts";
 
 export default function AddDialog() {
     const navigate = useNavigate();
+    
+    const { setSeverity, setMessage } = useContext(AlertContext);
+    const { db, storage } = useContext(FirebaseContext);
+    
+    const onSubmit = (params: {
+        updates: Partial<Task>,
+        uploads: Array<File>
+    }) => {
+        const { updates, uploads } = params;
+        
+        uploads.forEach((file) => {
+            uploadAttach(storage, file)
+                .catch((error) => {
+                    setSeverity(error.severity);
+                    setMessage(error.message);
+                });
+        });
+    
+        addTask(db, updates)
+            .then((task) => {
+                setSeverity("success"); 
+                setMessage("Atividade editada com sucesso");
+                    
+                setTimeout(() => (
+                    navigate(`/task/${task.uuid}`, { 
+                       state: { enableLoader: true } 
+                    })
+                ), 4000);
+            })
+            .catch((error) => {
+                setSeverity(error.severity);
+                setMessage(error.message);
+            });
+    }
     
     return (
         <Dialog
@@ -15,7 +63,10 @@ export default function AddDialog() {
                 navigate("/", { state: { enableLoader: false }})
             )}
         >
-            <TaskForm formTitle="Adicionar atividade"/>
+            <TaskForm 
+                formTitle="Adicionar atividade"
+                onSubmit={onSubmit}
+            />
         </Dialog>
     );
 };
