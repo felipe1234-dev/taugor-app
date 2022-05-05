c9a1e5eaf8b7d0eb8ac20716d3b68cbce1e23982
Felipe Alves <felipejean2002@gmail.com>
2022-05-05 01:35:12 -0300

    Exportando addTask

--- src/api/collections/Tasks/index.ts
+++ src/api/collections/Tasks/index.ts
@@ -1,3 +1,4 @@
+export { default as addTask } from "./addTask";
 export { default as deleteTask } from "./deleteTask";
 export { default as getTasks } from "./getTasks";
 export { default as getTask } from "./getTask";
