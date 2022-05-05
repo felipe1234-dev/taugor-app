01e660a223d3f40b6ef9724fcc06f6026d2d9ca0
Felipe Alves <felipejean2002@gmail.com>
2022-05-04 22:10:07 -0300

    Corrigindo bug onde o cursor volta pro início ao escrever no editor, por causa de re-renderização devido ao useEffect

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,57 +1,43 @@
 // Libs
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Box } from "@mui/material";
-import RichTextEditor, { EditorValue } from "react-rte";
+import RichTextEditor, { EditorValue, Props as RichTextEditorProps } from "react-rte";
 
 // Styles
 import "@local/style/components/TextEditor.scss";
 
 // Props interface
 export interface TextEditorProps {
-    name?: string,
-    initialContent?: string,
-    readOnly?: boolean,
-    onChange?: (html: string) => void
+    onChange?: (html: string) => void,
+    initialContent?: string
 };
 
 export default function TextEditor({
-    name,
     initialContent = "<h1>Olá, mundo!</h1>", 
     readOnly = false, 
+    placeholder,
     onChange = () => {}
-}: TextEditorProps) {
+}: TextEditorProps & RichTextEditorProps) {
     const [content, setContent] = useState<EditorValue>(
-        RichTextEditor.createValueFromString("", "html")
+        RichTextEditor.createValueFromString(initialContent, "html")
     );
-    
-    useEffect(() => {
-        setContent(
-            RichTextEditor.createValueFromString(initialContent, "html")
-        );
-    }, [initialContent])
     
     return (
         <div className="TextEditor">
             {!readOnly? (
-                <>
-                    <textarea
-                        name={name}
-                        defaultValue={content.toString("html")}
-                        style={{ display: "none" }}
-                    />
-                    <RichTextEditor
-                        value={content}
-                        onChange={(newContent: EditorValue) => {
-                            setContent(newContent);
-                            onChange(newContent.toString("html"));
-                        }}
-                    />
-                </>
+                <RichTextEditor
+                    placeholder={placeholder}
+                    value={content || RichTextEditor.createEmptyValue()}
+                    onChange={(newContent: EditorValue) => {
+                        setContent(newContent);
+                        onChange(newContent.toString("html"));
+                    }}
+                />
             ) : (
                 <Box 
                     className="TextEditor-content"
                     dangerouslySetInnerHTML={{
-                        __html: content.toString("html")
+                        __html: content?.toString("html") || ""
                     }}
                 />
             )}
