dcd985c8a52c2577de65848c09c410829cbf2f51
Felipe Alves <felipejean2002@gmail.com>
2022-05-05 22:36:43 -0300

    Exportando useQueryParams

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useOnMobile } from "./useOnMobile";
 export { default as useOnScreen } from "./useOnScreen";
 export { default as useOnScroll } from "./useOnScroll";
+export { default as useQueryParams } from "./useQueryParams";
