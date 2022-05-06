d33ff315e1c611d5a19bf84ee3acafe19c7331b3
Felipe Alves <felipejean2002@gmail.com>
2022-05-06 17:59:52 -0300

    Centralizando o formulário de login

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,9 +1,5 @@
 // Libs
-import { 
-	Grid, 
-	Container, 
-	Box
-} from "@mui/material";
+import { Box } from "@mui/material";
 
 // Page components
 import Heading from "./Heading";
@@ -13,27 +9,20 @@ import Form from "./Form";
 import "@local/style/pages/LoginPage.scss";
 
 export default function LoginPage() {
-	return ( 
-		<Grid
-            justifyContent="center"
-            alignItems="center"
-        >
-			<Grid item>
-				<Container
-                    className="LoginPage"
-                    maxWidth="xs"
-                >
-					<Box
-                        display="flex"
-                        flexDirection="column"
-                        alignItems="center"
-                        sx={{ marginTop: 8 }}
-                    >
-                        <Heading />
-						<Form />
-					</Box>
-				</Container>
-			</Grid>
-		</Grid>
+	return (
+		<Box
+			className="LoginPage"
+			display="flex"
+			flexDirection="column"
+			alignItems="center"
+			justifyContent="center"
+			sx={{
+				minHeight: "100vh",
+				minWidth: "100%"
+			}}
+		>
+			<Heading />
+			<Form />
+		</Box>
 	);
 };
