645e74995f8cbf68137eea7c607608b1eddd73a3
Felipe Alves <felipejean2002@gmail.com>
2022-05-06 16:30:34 -0300

    Criando componente de carregamento com Skeleton do MUI

---
+++ src/pages/home/Body/Loading.tsx
@@ -0,0 +1,55 @@
+import { 
+    List, 
+    ListItem, 
+    ListItemIcon, 
+    Skeleton, 
+    ListItemText, 
+    Typography, 
+    Grid 
+} from "@mui/material";
+
+export default function Loading() {
+    return (
+        <List>
+            {[...Array(10).keys()].map((item, i) => (
+                <ListItem key={i}>
+                    <ListItemIcon>
+                        <Skeleton
+                            variant="circular"
+                            width={40}
+                            height={40}
+                        />
+                    </ListItemIcon>
+                    <ListItemText
+                        primary={(
+                            <Typography
+                                component="div"
+                                variant="h4"
+                            >
+                                <Skeleton width={300} />
+                            </Typography>
+                        )}
+                        secondary={(
+                            <>
+                                <Typography
+                                    component="div"
+                                    variant="body2"
+                                >
+                                    <Skeleton width={260} />
+                                </Typography>
+                                <Grid container spacing={1}>
+                                    <Grid item>
+                                        <Skeleton width={180} />
+                                    </Grid>
+                                    <Grid item>
+                                        <Skeleton width={180} />
+                                    </Grid>
+                                </Grid>
+                            </>
+                        )}
+                    />
+                </ListItem>
+            ))}
+        </List>
+    );
+};
