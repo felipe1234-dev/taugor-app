6d311ad24a3db71341555529b037737a5579c4bc
Felipe Alves <felipejean2002@gmail.com>
2022-05-07 01:40:40 -0300

    feat(Exportando stripTags): Função de remover tags (semelhante ao innerText)

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -4,4 +4,5 @@ export { default as isLocation } from "./isLocation";
 export { default as isRouteState } from "./isRouteState";
 export { default as stringToAvatar } from "./stringToAvatar";
 export { default as stringToColor } from "./stringToColor";
+export { default as stripTags } from "./stripTags";
 export { default as translateDate } from "./translateDate";
